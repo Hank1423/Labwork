45c5b3f20d25783d63e60ebd02b42cec63ec6581
Hank1423 <ngohank853@gmail.com>
2026-04-13 15:52:26 +0700

    labwork

--- Lesson 9/main5.js
+++ Lesson 9/main5.js
@@ -3,23 +3,7 @@ document.getElementById("convertForm").addEventListener("submit", function(event
     let amount = parseFloat(document.getElementById("amount").value);
     let from = document.getElementById("from").value;
     let to = document.getElementById("to").value;
-    let result = 0;
-    if (isNaN(amount)) {
-        alert("Please enter a valid number for the amount.");
-        return;
-    }
-    if (from === "VND" && to === "USD") {
-        result = amount / 23000;
-    } else if (from === "USD" && to === "VND") {
-        result = amount * 23000;
-    } else if (from === "VND" && to === "EUR") {
-        result = amount / 27500;
-    } else if (from === "EUR" && to === "VND") {
-        result = amount * 27500;
-    } else if (from === "VND" && to === "JPY") {
-        result = amount / 165;
-    } else if (from === "JPY" && to === "VND") {
-        result = amount * 165;
-    }
-    document.getElementById("result").textContent = "Result: " + result.toFixed(2) + " " + to;
+    
+    let result = (amount * to) / from;
+    document.getElementById("result").textContent = "Result: " + result.toFixed(2);
 });
